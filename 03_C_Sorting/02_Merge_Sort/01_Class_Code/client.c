673684869ff609ce7177af3e4ed1b6fec8e40920
nehetegaurav3 <nehetegaurav3@gmail.com>
2023-07-23 18:00:29 +0530

    06:00 PM - 23/07/2023

---
+++ 03_C_Sorting/02_Merge_Sort/01_Class_Code/client.c
@@ -0,0 +1,71 @@
+
+/*
+    @file: client.c
+    @brief: Client side file for usage of Merge Sort Algorithm
+    @author: Gaurav Nehete(nehetegaurav3@gmail.com)
+    @date: 20 July 2023 (Thursday) 01:22 PM  
+*/
+#include	<stdio.h>
+#include	<stdlib.h>
+
+#include	"mv_dcll.h"
+
+p_mv_dcll_t list = NULL;
+
+void print_list(p_mv_dcll_t list)
+{
+	//Code
+	fprintf(stdout, "List: {START}->");
+	for(long long le = 0; le < MVDCLLSize(list); ++le)
+	{
+		fprintf(stdout, "{%lld}->", (long long)MVDCLLDataAt(list, le));
+	}
+	fprintf(stdout, "{END}\n\n");
+}
+
+status_t compare_equality(data_t data1, data_t data2)
+{
+	//Code
+	if((long long)data1 == (long long)data2)
+	{
+		return(SUCCESS);
+	}
+
+	return(FAILURE);
+}
+
+status_t compare_inequality(data_t data1, data_t data2)
+{
+	//Code
+	if((long long)data1 < (long long)data2)
+	{
+		return(SUCCESS);
+	}
+
+	return(FAILURE);
+}
+
+void delete_data(data_t data)
+{
+	fprintf(stdout, "Deleting Data = %lld\n", (long long)data);
+}
+
+int main(void)
+{
+	//Code
+	list = MVCreateDCLL();
+
+	for(long long le = 1; le <= 10; ++le)
+	{
+		MVDCLLInsertFront(list, (data_t)(le * 10));
+	}
+	fprintf(stdout, "After MVDCLLInsertFront:\n");
+	print_list(list);
+
+	MVDCLLMergeSort(list, compare_inequality);
+	fprintf(stdout, "After List Sort:\n");
+	print_list(list);
+	
+	exit(SUCCESS);
+}
+
---
+++ 03_C_Sorting/02_Merge_Sort/01_Class_Code/mv_mergesort.h
@@ -0,0 +1,49 @@
+
+/*
+	@file: mv_mergesort.h
+	@brief: Header file for declaration of Type Definitions and Interface Functions For Merge Sort Implementation
+	@author: Gaurav Nehete (nehetegaurav3@gmail.com)
+	@date: 20 July 2023 (Thursday) 11:33 AM
+*/
+
+//Conditional Compilation
+#ifndef     _MV_MERGE_SORT_H	
+#define     _MV_MERGE_SORT_H
+
+#ifndef     DEFS
+#define     DEFS
+
+#define     SUCCESS     0
+#define     FAILURE     1
+
+//Typedefs
+typedef 	void*	data_t;
+typedef 	int 	status_t;
+
+typedef     status_t(*COMPAREDATAPROC)(data_t, data_t);
+typedef     data_t(*GETDATAPROC)(void* ds, long long index);
+typedef     data_t(*SETDATAPROC)(void* ds, data_t data, long long index);
+
+#endif      /* DEFS */
+
+typedef		void*(*CREATECOPYPROC)(void*, long long, long long);
+typedef		void(*DELETEPROC)(void** ds);
+
+/*
+    @function: To Implement Merge Sort Algorithm
+    @identifier: MVMergeSort
+    @params:	1. Data Struct
+  		    	2. First Index
+  			    3. Last Index
+     			4. Compare Data Proc
+  	    		5. Get Data Proc
+  	    		6. Set Data Proc
+  		    	7. Copy Proc
+  			    8. Delete Data Structure Proc
+    @returns: void
+*/
+extern void MVMergeSort(void* ds, long long first, long long last, 
+				 		COMPAREDATAPROC pcompareproc, GETDATAPROC pgetdataproc, SETDATAPROC psetdataproc, 
+				 		CREATECOPYPROC pcreatecopyproc, DELETEPROC pdeletproc);
+
+#endif      /* _MV_MERGE_SORT_H */
